4c2317b15a4e5f0021b4be726d2f15402d360451
LiloTM <Laura-U@kabelmail.de>
2022-02-14 22:57:45 +0100

    refactored code

--- Assets/Scripts/CrawlerScript.cs
+++ Assets/Scripts/CrawlerScript.cs
@@ -9,55 +9,48 @@ public class CrawlerScript : MonoBehaviour
     private GameObject Foot;
     private GameObject Leg;
     float threshold;
-
+    Vector3 newLegScale;
+    RaycastHit hit;
+    
     void Start()
     {
         threshold = Random.Range(1.0f, 2.0f);
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity))
-        {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity)) {
             Foot = Instantiate(FootPrefab, hit.point, Quaternion.LookRotation(hit.normal));
-
-            Leg = Instantiate(LegPrefab, hit.point, Quaternion.identity);
-            
-            Vector3 newScale = Leg.transform.localScale;
-            newScale.z = Vector3.Distance(transform.position, hit.point);
-            Leg.transform.localScale = newScale;
-            Leg.transform.LookAt(transform.position);
+            Leg = Instantiate(LegPrefab, hit.point, Quaternion.identity);  
+            LegUpdate();
         }
-        else{
+        else {
             Foot = Instantiate(FootPrefab, transform.position, Quaternion.identity);
-
             Leg = Instantiate(LegPrefab, transform.position, Quaternion.identity);
         }
-        
     }
 
     void Update()
     {
-        Vector3 newScale = Leg.transform.localScale;
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity)/* && !hit.collider.gameObject.Leg && !hit.collider.gameObject.Foot*/)
-        {
-            // if distance between foot and raycast downward bigger than value
+        LegUpdate();
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity)) {
+            // updates if the distance between foot and box bigger than value
             float distance = (hit.point - Foot.transform.position).magnitude;
             if(distance > threshold){
+                // creates a new foot
                 Destroy(Foot); 
                 Foot = Instantiate(FootPrefab, hit.point, Quaternion.LookRotation(hit.normal));
                 threshold = Random.Range(1.0f, 2.0f);
 
+                // creates a new leg
                 Destroy(Leg); 
                 Leg = Instantiate(LegPrefab, hit.point, Quaternion.identity);
-                newScale.z = Vector3.Distance(transform.position, Foot.transform.position);
-                Leg.transform.localScale = newScale;
+                LegUpdate();
             }
         }
-
-        newScale.z = Vector3.Distance(transform.position, Foot.transform.position);
-        Leg.transform.localScale = newScale;
-        Leg.transform.LookAt(transform.position);
-
     }
-    // TODO: Legs make me cry
+
+    void LegUpdate()
+    {   //updates length and rotation of the legs 
+        newLegScale = Leg.transform.localScale;
+        newLegScale.z = Vector3.Distance(transform.position, Foot.transform.position);
+        Leg.transform.localScale = newLegScale;
+        Leg.transform.LookAt(transform.position);
+    }
 }
