d1a78d716b438c76a01e4312739fd5e14391a255
LiloTM <Laura-U@kabelmail.de>
2022-02-15 16:08:02 +0100

    added firstPersonCamera functionality

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,14 @@ public class CameraController : MonoBehaviour
     public Camera cam1;
     public Camera cam2;
 
+    public float mouseSensitivity = 150f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
     void Start()
     {
         cam1.enabled = true;
-        cam2.enabled = false;
+        cam2.enabled = false; 
     }
 
     void Update()
@@ -18,6 +22,21 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C)) {
             cam1.enabled = !cam1.enabled;
             cam2.enabled = !cam2.enabled;
+            
+        }
+        if(cam1.enabled == true){
+            Cursor.lockState = CursorLockMode.None;
+        }
+        if(cam2.enabled == true){
+            Cursor.lockState = CursorLockMode.Locked;
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
         }
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    Vector3 gravityVector;
+
+    void Update()
+    {      
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        gravityVector.y += gravity * Time.deltaTime;
+        controller.Move(gravityVector * Time.deltaTime);
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * Time.deltaTime * speed);
+        
+    }
+}
